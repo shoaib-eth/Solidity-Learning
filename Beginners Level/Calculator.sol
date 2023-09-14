a1fa7077411a232fa66a0fca5e913289c458f57d
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-09-14 16:13:36 +0530

    Initial commit

---
+++ Beginners Level/Calculator.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Calculator {
+    uint256 result = 0;
+
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function get() public view returns (uint256) {
+        return result;
+    }
+}
