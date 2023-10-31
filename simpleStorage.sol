fcccf7dd4733dd0ec598e9800ad4e1a917ef50ec
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-10-31 15:35:21 +0530

    Updated simpleStorage.sol

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,33 +1,20 @@
-// I'm a comment!
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.8;
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.17;
 
 contract SimpleStorage {
 
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
 
-    struct People {
+    struct Person {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
-    People[] public people;
+
+    Person[] public people;
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
-    
-    function retrieve() public view returns (uint256){
-        return favoriteNumber;
-    }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-}
+    function retrieve() public view returns (uint256) {
