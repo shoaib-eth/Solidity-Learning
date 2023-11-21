c98b5dfe5d747c9afe5f1c5a0a363c291457ee2f
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-21 17:15:48 +0530

    Initial commit

--- storageFactory.sol
+++ storageFactory.sol
@@ -2,4 +2,12 @@
 
 pragma solidity 0.8.19;
 
-import 
+import {SimpleStorage, SimpleStorage1, SimpleStorage2, SimpleStorage3} from "./simpleStorage.sol";
+
+contract storageFactory {
+    SimpleStorage public mySimpleStorageContract;
+
+    function createSimpleStorage() public {
+        mySimpleStorageContract = new SimpleStorage();
+    }
+}
