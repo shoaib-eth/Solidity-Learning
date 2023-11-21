669aa1ad4839dc9496e125eb1277362b67abf7c9
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-21 17:17:44 +0530

    Initial commit

--- storageFactory.sol
+++ storageFactory.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity 0.8.19; 
 
-import {SimpleStorage, SimpleStorage1, SimpleStorage2, SimpleStorage3} from "./simpleStorage.sol";
+import {SimpleStorage, SimpleStorage1, SimpleStorage2, SimpleStorage3} from "./simpleStorage.sol";  // import the contract SimpleStorage from the file simpleStorage.sol 
 
 contract storageFactory {
-    SimpleStorage public mySimpleStorageContract;
+    SimpleStorage public mySimpleStorageContract; // create a variable of type SimpleStorage and call it mySimpleStorageContract 
 
     function createSimpleStorage() public {
-        mySimpleStorageContract = new SimpleStorage();
+        mySimpleStorageContract = new SimpleStorage(); // create an instance of SimpleStorage and assign it to the variable mySimpleStorageContract 
     }
 }
