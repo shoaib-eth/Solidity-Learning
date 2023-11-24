b70e4603df530a76e180d2a3a69b1cc9cb18d5f6
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-24 17:05:04 +0530

    Initial commit

--- cryptoZombies/lesson - 01/zombieFactory.sol
+++ cryptoZombies/lesson - 01/zombieFactory.sol
@@ -5,7 +5,7 @@ contract ZombieFactory {
     event NewZombie(uint256 zombieId, string name, uint256 dna);
 
     uint256 dnaDigits = 16;
-    uint256 dnaModulus = 10**dnaDigits;
+    uint256 dnaModulus = 10 ** dnaDigits;
 
     struct Zombie {
         string name;
@@ -20,7 +20,9 @@ contract ZombieFactory {
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string memory _str) private view returns (uint256) {
+    function _generateRandomDna(
+        string memory _str
+    ) private view returns (uint256) {
         uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
