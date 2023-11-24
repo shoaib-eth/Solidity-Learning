aaa935d24f3bd84b0446009548ba57606e9b6f66
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-24 17:02:59 +0530

    Initial commit

--- cryptoZombies/lesson - 01/zombieFactory.sol
+++ cryptoZombies/lesson - 01/zombieFactory.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 contract ZombieFactory {
-    event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+    uint256 dnaDigits = 16;
+    // Add missing function declaration
+    function _createZombie(string memory _name, uint256 _dna) private {
+        // Function implementation goes here
+    uint256 dnaModulus = 10**dnaDigits;
 
     struct Zombie {
         string name;
@@ -14,20 +17,19 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string memory _name, uint256 _dna) private {
+        uint256 id = zombies.length;
+        zombies.push(Zombie(_name, _dna));
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(
-        string memory _str
-    ) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
+    function _generateRandomDna(string memory _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string memory _name) public {
-        uint randDna = _generateRandomDna(_name);
+        uint256 randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 }
