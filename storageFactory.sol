3cf83ed99cd801228793b84bd022646a483860dc
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-24 16:59:26 +0530

    Initial commit

--- storageFactory.sol
+++ storageFactory.sol
@@ -6,15 +6,16 @@ pragma solidity ^0.8.19;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage[] public listOfSimpleStorageContracts;  // array of SimpleStorage contracts. Each contract is an instance of SimpleStorage contract.
+    SimpleStorage[] public listOfSimpleStorageContracts; // array of SimpleStorage contracts. Each contract is an instance of SimpleStorage contract.
 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorageContractVariable = new SimpleStorage();  
+        SimpleStorage simpleStorageContractVariable = new SimpleStorage();
         // SimpleStorage simpleStorage = new SimpleStorage();
-        listOfSimpleStorageContracts.push(simpleStorageContractVariable); // push the contract instance to the array 
-    } 
+        listOfSimpleStorageContracts.push(simpleStorageContractVariable); // push the contract instance to the array
+    }
 
     function sfStore(
+        // sfStore is a function that stores a number in a SimpleStorage contract
         uint256 _simpleStorageIndex,
         uint256 _simpleStorageNumber
     ) public {
@@ -23,11 +24,12 @@ contract StorageFactory {
         // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
         listOfSimpleStorageContracts[_simpleStorageIndex].store(
             _simpleStorageNumber
-        );
+        ); // store the number in the SimpleStorage contract
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // sfGet is a function that gets a number from a SimpleStorage contract
         // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve(); // retrieve the number from the SimpleStorage contract
     }
 }
