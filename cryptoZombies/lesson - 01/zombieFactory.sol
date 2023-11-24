2b43f3e7aaf017c05d8328069ca4ff22c714d823
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-24 17:00:08 +0530

    Initial commit

--- cryptoZombies/lesson - 01/zombieFactory.sol
+++ cryptoZombies/lesson - 01/zombieFactory.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 contract ZombieFactory {
     event NewZombie(uint zombieId, string name, uint dna);
 
-    uint dnaDigits = 16; 
-    uint dnaModulus = 10 ** dnaDigits; 
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie { 
+    struct Zombie {
         string name;
         uint256 dna;
     }
