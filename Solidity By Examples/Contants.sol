ed0aecda1a2c412b35ed6ded757f9c12f610f093
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-25 16:19:29 +0530

    Initial commit

--- Solidity By Examples/Contants.sol
+++ Solidity By Examples/Contants.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.00;
 
 contract Constants {
     // coding convention to uppercase constant variables
-    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
     uint public constant MY_UINT = 123;
 }
 
