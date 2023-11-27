1bd1f4fea5d6d3e11e5ec22f99b1dde4fee248b1
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-27 17:36:51 +0530

    Initial commit

---
+++ Solidity By Examples/Transaction.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Transaction {
+    address public buyer;
+    uint public amount;
+    uint public etherPrice;
+
+    constructor() {
+        buyer = msg.sender;
+        amount = 0;
+        etherPrice = 0;
+    }
+
+    function buyEther(uint _amount, uint _etherPrice) external payable {
+        require(msg.value == _amount * _etherPrice, "Incorrect amount of Ether sent");
+        amount += _amount;
+        etherPrice = _etherPrice;
+    }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+}
