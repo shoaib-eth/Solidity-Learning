73a746bfcc6958eedf442c6255e1867e2ff9c0ce
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-27 17:38:37 +0530

    Initial commit

--- Solidity By Examples/Transaction.sol
+++ Solidity By Examples/Transaction.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract Transaction {
-    address public buyer;
-    uint public amount;
-    uint public etherPrice;
+    address public buyer; // Address of the buyer
+    uint public amount; // Amount of ether bought
+    uint public etherPrice; // Price of ether
 
     constructor() {
-        buyer = msg.sender;
-        amount = 0;
-        etherPrice = 0;
+        buyer = msg.sender; // Set the buyer as the contract deployer
+        amount = 0; // Initialize the amount to 0
+        etherPrice = 0; // Initialize the ether price to 0
     }
 
     function buyEther(uint _amount, uint _etherPrice) external payable {
@@ -17,11 +18,11 @@ contract Transaction {
             msg.value == _amount * _etherPrice,
             "Incorrect amount of Ether sent"
         );
-        amount += _amount;
-        etherPrice = _etherPrice;
+        amount += _amount; // Add the bought amount to the total amount
+        etherPrice = _etherPrice; // Update the ether price
     }
 
     function getBalance() external view returns (uint) {
-        return address(this).balance;
+        return address(this).balance; // Return the contract's balance
     }
 }
