4bcf43f88e0f6d9c3f77596f90072eea2bebfd21
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-11 17:10:14 +0530

    Added Result.sol

--- Result.sol
+++ Result.sol
@@ -10,12 +10,18 @@ contract Result {
     mapping(address => ExamResult) public results;
 
     function publishResult(address student, uint256 marks) public {
-        require(!results[student].isPublished, "Result already published for this student");
+        require(
+            !results[student].isPublished,
+            "Result already published for this student"
+        );
         results[student] = ExamResult(marks, true);
     }
 
     function getMarks(address student) public view returns (uint256) {
-        require(results[student].isPublished, "Result not published for this student");
+        require(
+            results[student].isPublished,
+            "Result not published for this student"
+        );
         return results[student].marks;
     }
 }
