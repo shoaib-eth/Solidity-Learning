0012ee9957bda53c5f930882caaafad9ffee1bec
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-11 17:09:46 +0530

    Initial commit

---
+++ Result.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Result {
+    struct ExamResult {
+        uint256 marks;
+        bool isPublished;
+    }
+
+    mapping(address => ExamResult) public results;
+
+    function publishResult(address student, uint256 marks) public {
+        require(!results[student].isPublished, "Result already published for this student");
+        results[student] = ExamResult(marks, true);
+    }
+
+    function getMarks(address student) public view returns (uint256) {
+        require(results[student].isPublished, "Result not published for this student");
+        return results[student].marks;
+    }
+}
