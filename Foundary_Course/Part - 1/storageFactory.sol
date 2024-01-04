9f9f31eb024e80de03c768f60155abbf3a7dbf8f
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-04 18:01:15 +0530

    Initial commit

--- Foundary_Course/Part - 1/storageFactory.sol
+++ Foundary_Course/Part - 1/storageFactory.sol
@@ -1,35 +1,26 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.19;
 
-// import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
-import {SimpleStorage} from "./SimpleStorage.sol";
+import {simpleStorage} from "./simpleStorage.sol";
 
-contract StorageFactory {
-    SimpleStorage[] public listOfSimpleStorageContracts; // array of SimpleStorage contracts. Each contract is an instance of SimpleStorage contract.
+contract storageFactory {
+    simpleStorage[] public listOfSimpleStorageContract;
 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorageContractVariable = new SimpleStorage();
-        // SimpleStorage simpleStorage = new SimpleStorage();
-        listOfSimpleStorageContracts.push(simpleStorageContractVariable); // push the contract instance to the array
+        simpleStorage simpleStorageContractVariable = new simpleStorage();
+        listOfSimpleStorageContract.push(simpleStorageContractVariable);
     }
 
-    function sfStore(
-        // sfStore is a function that stores a number in a SimpleStorage contract
-        uint256 _simpleStorageIndex,
-        uint256 _simpleStorageNumber
-    ) public {
-        // Address
-        // ABI
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
-        listOfSimpleStorageContracts[_simpleStorageIndex].store(
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        listOfSimpleStorageContract[_simpleStorageIndex].store(
             _simpleStorageNumber
-        ); // store the number in the SimpleStorage contract
+        );
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // sfGet is a function that gets a number from a SimpleStorage contract
-        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve(); // retrieve the number from the SimpleStorage contract
+        return
+            listOfSimpleStorageContract[_simpleStorageIndex].retrieveNumber();
     }
 }
