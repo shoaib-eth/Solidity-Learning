7decd8b467e80bd2e01d5cc468b51677541251f2
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-04 18:04:51 +0530

    Initial commit

--- Foundary_Course/Part - 1/Inheritance.sol
+++ Foundary_Course/Part - 1/Inheritance.sol
@@ -4,7 +4,11 @@ pragma solidity 0.8.19;
 import {simpleStorage} from "./simpleStorage.sol";
 
 contract InheritContract is simpleStorage {
-    function store(uint) public override {
+    function store(uint256) public override {
         myNumber = myNumber + 5;
+    }
+
+    function sayHello() public pure returns (string memory) {
+        return "Hello!";
     }
 }
