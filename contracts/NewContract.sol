70f4c0b38f2f0fdd77ea27804ef3e889c877ffd8
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-05 18:11:27 +0530

    Add getData function to NewContract

--- contracts/NewContract.sol
+++ contracts/NewContract.sol
@@ -7,4 +7,8 @@ contract NewContract {
     function sendData(string memory _data) public {
         data = _data;
     }
+
+    function getData() public view returns (string memory) {
+        return data;
+    }
 }
