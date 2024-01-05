f3897be64c37c4703d71083527d2eb57256230eb
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-05 18:10:39 +0530

    Initial commit

--- contracts/NewContract.sol
+++ contracts/NewContract.sol
@@ -1,18 +1,9 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract NewContract {
-    mapping(uint256 => string) public myMap;     
+    string public data;
 
-    function addValue(uint256 key, string memory value) public {
-        myMap[key] = value;
-    }
-
-    function getValue(uint256 key) public view returns (string memory) {
-        return myMap[key];
-    }
-
-    function deleteValue(uint256 key) public {
-        delete myMap[key];
+    function sendData(string memory _data) public {
+        data = _data;
     }
 }
