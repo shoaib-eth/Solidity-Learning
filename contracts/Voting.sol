baad657059e48740de1f0817d76215f05ed3aa12
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-14 15:18:47 +0530

    Initial commit

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.19;
 
 contract Voting {
-    mapping (bytes32 => uint8) public votesReceived;
+    mapping(bytes32 => uint8) public votesReceived;
     bytes32[] public candidateList;
 
     constructor(bytes32[] memory candidateNames) {
         candidateList = candidateNames;
     }
 
-    function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+    function totalVotesFor(bytes32 candidate) public view returns (uint8) {
         require(validCandidate(candidate));
         return votesReceived[candidate];
     }
@@ -19,8 +19,8 @@ contract Voting {
         votesReceived[candidate] += 1;
     }
 
-    function validCandidate(bytes32 candidate) view public returns (bool) {
-        for(uint i = 0; i < candidateList.length; i++) {
+    function validCandidate(bytes32 candidate) public view returns (bool) {
+        for (uint i = 0; i < candidateList.length; i++) {
             if (candidateList[i] == candidate) {
                 return true;
             }
