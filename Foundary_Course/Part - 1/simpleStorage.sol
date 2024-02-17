dc8ed6f137be0d035b049149fa61f755317711f6
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-17 17:45:00 +0530

    Added Comments

--- Foundary_Course/Part - 1/simpleStorage.sol
+++ Foundary_Course/Part - 1/simpleStorage.sol
@@ -10,18 +10,23 @@ contract simpleStorage {
         uint age;
     }
 
+    // Array of Person struct type 
     Person[] public listOfPeoples;
 
+    // Mapping of name to age 
     mapping(string => uint) public nameToAge;
 
+    // Store a number
     function store(uint _myNumber) public virtual {
         myNumber = _myNumber;
     }
 
+    // Retrieve the stored number
     function retrieveNumber() public view returns (uint) {
         return myNumber;
     }
 
+    // Add a person to the list of people and store their name and age in the mapping 
     function addPerson(string memory _name, uint _age) public {
         listOfPeoples.push(Person(_name, _age));
         nameToAge[_name] = _age;
