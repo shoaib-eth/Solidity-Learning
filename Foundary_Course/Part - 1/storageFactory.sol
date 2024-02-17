a21f7a077b24b04d60e5d06dfe00e847e77ac7c7
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-17 17:43:14 +0530

    Added Comments

--- Foundary_Course/Part - 1/storageFactory.sol
+++ Foundary_Course/Part - 1/storageFactory.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.19;
 import {simpleStorage} from "./simpleStorage.sol";
 
 contract storageFactory {
+    // List of simpleStorage contracts
     simpleStorage[] public listOfSimpleStorageContract;
 
+    // Create a new simpleStorage contract and store it in the list
     function createSimpleStorageContract() public {
         simpleStorage simpleStorageContractVariable = new simpleStorage();
         listOfSimpleStorageContract.push(simpleStorageContractVariable);
     }
 
+    // Store a number in a simpleStorage contract
     function sfStore(
         uint256 _simpleStorageIndex,
         uint256 _simpleStorageNumber
@@ -20,6 +23,7 @@ contract storageFactory {
         );
     }
 
+    // Retrieve a number from a simpleStorage contract
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         return
             listOfSimpleStorageContract[_simpleStorageIndex].retrieveNumber();
