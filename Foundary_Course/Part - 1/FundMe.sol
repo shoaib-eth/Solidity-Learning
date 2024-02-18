c016b555c1c8cbc4195dff9dd6834bc50bd8bb41
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-18 17:30:22 +0530

    Initial commit

--- Foundary_Course/Part - 1/FundMe.sol
+++ Foundary_Course/Part - 1/FundMe.sol
@@ -4,12 +4,14 @@ pragma solidity 0.8.24;
 contract FundMe {
     uint256 public myValue;
 
+    // payable is a modifier that allows a function to receive ether
     function fund() public payable {
         myValue = myValue + 1;
 
         require(msg.value >= 0.01 ether, "Minimum contribution is 0.01 ether");
     }
 
+    // transfer the balance of the contract to the caller
     function withdraw() public {
         payable(msg.sender).transfer(address(this).balance);
     }
