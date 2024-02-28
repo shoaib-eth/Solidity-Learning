3e2fcba9e76058561bcec17aef04a4a774532804
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-28 16:48:53 +0530

    Initial commit

---
+++ Solidity By Examples/FundMe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract FundMe {
+    uint256 public minimumUSD = 5e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value >= minimumUSD, "Didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] =
+            addressToAmountFunded[msg.sender] +
+            msg.value;
+    }
+}
