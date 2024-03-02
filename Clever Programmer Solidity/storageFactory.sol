0e6f49836bf55dd40c36435ae0872fbe23774f72
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-02 17:47:53 +0530

    Initial commit

---
+++ Clever Programmer Solidity/storageFactory.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "./simpleStorage.sol";
+
+contract storageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // Address
+        // ABI
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(
+            _simpleStorageNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return
+            SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
+                .retrieve();
+    }
+}
