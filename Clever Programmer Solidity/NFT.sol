1f27d50db01da9c53d4d5321bc3943d8c015a5d7
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-21 15:14:57 +0530

    Initial commit

---
+++ Clever Programmer Solidity/NFT.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    uint256 public tokenCounter;
+    mapping(uint256 => string) public _tokenURIs;
+
+    constructor() ERC721("NFT", "NFT") {
+        tokenCounter = 0;
+    }
+
+    function createNFT(address owner, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = tokenCounter;
+        _safeMint(owner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newItemId;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://nft-api-123.herokuapp.com/api/token/";
+    }
+}
