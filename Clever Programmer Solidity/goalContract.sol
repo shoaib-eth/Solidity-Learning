1402f9b3bb0f1ae50ea30fd5ba7bb8412157f36d
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-22 15:10:11 +0530

    Initial commit

--- Clever Programmer Solidity/goalContract.sol
+++ Clever Programmer Solidity/goalContract.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
 
 contract GoalContract {
     address public owner;
