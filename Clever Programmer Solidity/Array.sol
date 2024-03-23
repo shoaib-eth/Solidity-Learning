b2b4007eca267abc87fb19af91ca5500bab53a8a
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-23 15:35:16 +0530

    Initial commit

---
+++ Clever Programmer Solidity/Array.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract Array {
+    uint[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    function getNumber(uint _index) public view returns(uint) {
+        return numbers[_index];
+    }
+
+    function getNumbers() public view returns(uint[] memory) {
+        return numbers;
+    }
+
+    function pushNumber(uint _number) public {
+        numbers.push(_number);
+    }
+
+    function popNumber() public {
+        numbers.pop();
+    }
+
+    function getLength() public view returns(uint) {
+        return numbers.length;
+    }
+}
