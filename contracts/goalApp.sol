d765967e9ccc718630315659b312750ad3d4853f
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-08 14:56:08 +0530

    Initial commit

--- contracts/goalApp.sol
+++ contracts/goalApp.sol
@@ -12,7 +12,11 @@ contract goalApp {
 
     Goal[] public goals;
 
-    function createGoal(string memory _name, uint256 _target, uint256 _deadline) public {
+    function createGoal(
+        string memory _name,
+        uint256 _target,
+        uint256 _deadline
+    ) public {
         goals.push(Goal(_name, _target, 0, _deadline, msg.sender));
     }
 
@@ -22,13 +26,22 @@ contract goalApp {
 
     function contribute(uint256 _goalId, uint256 _amount) public {
         require(goals[_goalId].deadline > block.timestamp, "Goal has expired");
-        require(goals[_goalId].owner != msg.sender, "Owner cannot contribute to own goal");
-        require(goals[_goalId].current + _amount <= goals[_goalId].target, "Contribution exceeds target");
+        require(
+            goals[_goalId].owner != msg.sender,
+            "Owner cannot contribute to own goal"
+        );
+        require(
+            goals[_goalId].current + _amount <= goals[_goalId].target,
+            "Contribution exceeds target"
+        );
         goals[_goalId].current += _amount;
     }
 
     function withdraw(uint256 _goalId) public {
-        require(goals[_goalId].deadline < block.timestamp, "Goal has not expired");
+        require(
+            goals[_goalId].deadline < block.timestamp,
+            "Goal has not expired"
+        );
         require(goals[_goalId].owner == msg.sender, "Only owner can withdraw");
         payable(msg.sender).transfer(goals[_goalId].current);
         goals[_goalId].current = 0;
