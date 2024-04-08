9abbfbd78613c6ec3a94bc28603637d56328f3df
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-08 14:55:48 +0530

    Initial commit

---
+++ contracts/goalApp.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract goalApp {
+    struct Goal {
+        string name;
+        uint256 target;
+        uint256 current;
+        uint256 deadline;
+        address owner;
+    }
+
+    Goal[] public goals;
+
+    function createGoal(string memory _name, uint256 _target, uint256 _deadline) public {
+        goals.push(Goal(_name, _target, 0, _deadline, msg.sender));
+    }
+
+    function getGoals() public view returns (Goal[] memory) {
+        return goals;
+    }
+
+    function contribute(uint256 _goalId, uint256 _amount) public {
+        require(goals[_goalId].deadline > block.timestamp, "Goal has expired");
+        require(goals[_goalId].owner != msg.sender, "Owner cannot contribute to own goal");
+        require(goals[_goalId].current + _amount <= goals[_goalId].target, "Contribution exceeds target");
+        goals[_goalId].current += _amount;
+    }
+
+    function withdraw(uint256 _goalId) public {
+        require(goals[_goalId].deadline < block.timestamp, "Goal has not expired");
+        require(goals[_goalId].owner == msg.sender, "Only owner can withdraw");
+        payable(msg.sender).transfer(goals[_goalId].current);
+        goals[_goalId].current = 0;
+    }
+}
