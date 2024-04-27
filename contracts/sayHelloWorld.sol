7642aca378bbccdd4e193827ebdfb51ee0474192
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-27 16:11:53 +0530

    Initial commit

---
+++ contracts/sayHelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SayHelloWorld {
+    function sayHelloWorld() public pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
