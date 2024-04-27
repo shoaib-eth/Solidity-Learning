eb2879c5aadc7818df6eb499150b0aba6ed40823
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-27 16:10:53 +0530

    Initial commit

---
+++ contracts/operations.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Operations {
+    uint256 public a;
+    uint256 public b;
+
+    function set(uint256 _a, uint256 _b) public {
+        a = _a;
+        b = _b;
+    }
+
+    function add() public view returns (uint256) {
+        return a + b;
+    }
+
+    function subtract() public view returns (uint256) {
+        return a - b;
+    }
+
+    function multiply() public view returns (uint256) {
+        return a * b;
+    }
+
+    function divide() public view returns (uint256) {
+        return a / b;
+    }
+}
