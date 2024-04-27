3fffc605db03872952e47469ac6a52aecd238b98
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-27 16:09:45 +0530

    Initial commit

---
+++ contracts/sayHappyBirthday.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Birthday {
+    uint256 BirthMonth;
+    uint256 BirthDate;
+
+    function set(uint256 _BirthMonth, uint256 _BirthDate) public {
+        BirthMonth = _BirthMonth;
+        BirthDate = _BirthDate;
+    }
+
+    function sayHappyBirthday() public view returns (string memory) {
+        if (BirthMonth == 4 && BirthDate == 27) return "Happy birthday to you!";
+    }
+}
