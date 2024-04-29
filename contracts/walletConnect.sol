ba18b6a86f356bfce68bc77cac9efce3cc94ff9e
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-29 15:12:26 +0530

    Initial commit

--- contracts/walletConnect.sol
+++ contracts/walletConnect.sol
@@ -4,36 +4,36 @@ pragma solidity >=0.7.0 <0.9.0;
 contract walletConnect {
     address public owner;
     uint256 public balance;
-    
+
     constructor() {
         owner = msg.sender;
     }
-    
+
     function deposit() public payable {
         balance += msg.value;
     }
-    
+
     function withdraw(uint256 _amount) public {
         require(msg.sender == owner, "You are not the owner");
         require(_amount <= balance, "Insufficient balance");
         payable(owner).transfer(_amount);
         balance -= _amount;
     }
-    
-    function getBalance() public view returns(uint256) {
+
+    function getBalance() public view returns (uint256) {
         return balance;
     }
-    
+
     function transfer(address _to, uint256 _amount) public {
         require(_amount <= balance, "Insufficient balance");
         balance -= _amount;
         payable(_to).transfer(_amount);
     }
-    
-    function getOwner() public view returns(address) {
+
+    function getOwner() public view returns (address) {
         return owner;
     }
-    
+
     function setOwner(address _owner) public {
         require(msg.sender == owner, "You are not the owner");
         owner = _owner;
