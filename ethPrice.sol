53e058444f23790fff44339af2ead19675e617b1
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-05-10 19:35:47 +0530

    Initial commit

---
+++ ethPrice.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract ethPrice {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return answer;
+    }
+}
