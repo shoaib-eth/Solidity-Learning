31aa0c22db3aa7b789f94e701a0f676640224e9e
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-06-24 17:19:24 +0530

    Initial commit

---
+++ crowdSourcing.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract crowdSourcing {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return answer;
+    }
+
+    function getDecimals() public view returns (uint8) {
+        return priceFeed.decimals();
+    }
+
+    function getRoundData(uint80 _roundId)
+        public
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return priceFeed.getRoundData(_roundId);
+    }
+}
