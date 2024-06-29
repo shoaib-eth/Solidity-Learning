5e711a1b5b440e7e132bcffcde12c6377127ad33
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-06-29 16:09:03 +0530

    Initial commit

---
+++ contracts/TaskManagement.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract TaskManagement {
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    uint public taskCount = 0;
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(uint id, string content, bool completed);
+    event TaskCompleted(uint id, bool completed);
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
+}
