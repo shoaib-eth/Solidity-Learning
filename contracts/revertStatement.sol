61632f2e90ebface91c426923a2979317d517954
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-07-09 17:40:43 +0530

    Initial commit

---
+++ contracts/revertStatement.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+contract StorageOptimization {
+    uint256 public data = 10;
+
+    // This function will cost less gas
+    function increment() public {
+        data += 1;
+    }
+
+    // This function will cost more gas
+    function incrementBy(uint256 _value) public {
+        data += _value;
+    }
+
+    // This function will revert the transaction
+    function revertStatement() public {
+        require(data > 10, "Data is not greater than 10");
+    }
+}
