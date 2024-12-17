fe0f5f3988f0d4298a5b627d7f045d6957db37ad
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-12-17 16:39:56 +0530

    Add ageToName mapping and change solidity version

--- Foundary_Course/Part - 1/simpleStorage.sol
+++ Foundary_Course/Part - 1/simpleStorage.sol
@@ -1,34 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
 contract simpleStorage {
-    uint myNumber;
+    uint256 myNumber;
 
     // Array of a Person
     struct Person {
         string name;
-        uint age;
+        uint256 age;
     }
 
     // Array of Person struct type
     Person[] public listOfPeoples;
 
     // Mapping of name to age
-    mapping(string => uint) public nameToAge;
+    mapping(string => uint256) public nameToAge;
+
+    // Mapping of age to name
+    mapping(uint256 => string) public ageToName;
 
     // Store a number
-    function store(uint _myNumber) public virtual {
+    function store(uint256 _myNumber) public virtual {
         myNumber = _myNumber;
     }
 
     // Retrieve the stored number
-    function retrieveNumber() public view returns (uint) {
+    function retrieveNumber() public view returns (uint256) {
         return myNumber;
     }
 
     // Add a person to the list of people and store their name and age in the mapping
-    function addPerson(string memory _name, uint _age) public {
+    function addPerson(string memory _name, uint256 _age) public {
         listOfPeoples.push(Person(_name, _age));
         nameToAge[_name] = _age;
+        ageToName[_age] = _name;
     }
 }
