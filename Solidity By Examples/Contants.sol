ef37548a127f51f30d3af08e811e9a62774496ad
ShoaibDev69 <shoaib.developer69@gmail.com>
2025-03-04 16:27:22 +0530

    Initial commit

--- Solidity By Examples/Contants.sol
+++ Solidity By Examples/Contants.sol
@@ -4,29 +4,29 @@ pragma solidity ^0.8.0;
 contract Constants {
     // Address constant
     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
-    
+
     // Unsigned integer constant
-    uint public constant MY_UINT = 123;
-    
+    uint256 public constant MY_UINT = 123;
+
     // String constant
     string public constant MY_STRING = "Hello, Solidity!";
-    
+
     // Bytes32 constant
     bytes32 public constant MY_BYTES32 = keccak256(abi.encodePacked("Solidity"));
-    
+
     // Integer constant
-    int public constant MY_INT = -123;
-    
+    int256 public constant MY_INT = -123;
+
     // Boolean constant
     bool public constant MY_BOOL = true;
 
     // Function to demonstrate the use of constants
-    function getConstants() public pure returns (address, uint, string memory, bytes32, int, bool) {
+    function getConstants() public pure returns (address, uint256, string memory, bytes32, int256, bool) {
         return (MY_ADDRESS, MY_UINT, MY_STRING, MY_BYTES32, MY_INT, MY_BOOL);
     }
 
     // Function to demonstrate the use of a constant in a calculation
-    function multiplyByConstant(uint value) public pure returns (uint) {
+    function multiplyByConstant(uint256 value) public pure returns (uint256) {
         return value * MY_UINT;
     }
 
