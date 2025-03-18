def1207f3dde9556d7141de25f1b2a5dd9d6f02c
ShoaibDev69 <shoaib.developer69@gmail.com>
2025-03-18 13:44:30 +0530

    Initial commit

--- Solidity By Examples/L1Token.sol
+++ Solidity By Examples/L1Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract L1Token is ERC20 {
     constructor(uint256 initialSupply) ERC20("L1Token", "L1T") {
